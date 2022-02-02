1c2d7d0e40a07031643e0e6cbcf2c69817b066f0
Alex Martinez <alexmartinez0071234@gmail.com>
2022-02-02 17:17:51 -0500

    Timer Works but may need refactoring to account for the minutes to go past 60

--- client/src/Components/Timer.jsx
+++ client/src/Components/Timer.jsx
@@ -7,7 +7,7 @@ const Timer = () => {
     hours: 0,
     counter: 0,
   });
-  const { seconds, minutes, hours } = time;
+  const { seconds, minutes, hours, counter } = time;
 
   const secondTime = seconds < 10 ? `0${seconds}` : `${seconds}`;
   const minuteTime = minutes < 10 ? `0${minutes}` : `${minutes}`;
@@ -18,6 +18,38 @@ const Timer = () => {
   const toggle = () => {
     setIsActive(!isActive);
   };
+
+  const reset = () => {
+    setTime({
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      counter: 0,
+    });
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        const secondsCount = counter % 60;
+        const minuteCount = Math.floor(counter / 60);
+        const hoursCount = Math.floor(counter / 3600);
+
+        setTime((prevTime) => ({
+          seconds: secondsCount,
+          minutes: minuteCount,
+          hours: hoursCount,
+          counter: prevTime.counter + 1,
+        }));
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, time, counter]);
+
   return (
     <div>
       <p>Timer</p>
@@ -25,7 +57,7 @@ const Timer = () => {
       <p>{minuteTime}</p>
       <p>{hourTime}</p>
       <button onClick={toggle}>{isActive ? "Pause" : "Start"}</button>
-      <button>Reset</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
