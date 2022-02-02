75617d252c718460770717bcf4e3de835777d3db
Alex Martinez <alexmartinez0071234@gmail.com>
2022-02-02 15:46:45 -0500

    Basic logic setup for timer the rest still needs to be built out

--- client/src/Components/Timer.jsx
+++ client/src/Components/Timer.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Timer = () => {
+  const [time, setTime] = useState({
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    counter: 0,
+  });
+  const { seconds, minutes, hours } = time;
+
+  const secondTime = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  const minuteTime = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const hourTime = hours < 10 ? `0${hours}` : `${hours}`;
+
+  const [isActive, setIsActive] = useState(false);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
   return (
     <div>
       <p>Timer</p>
+      <p>{secondTime}</p>
+      <p>{minuteTime}</p>
+      <p>{hourTime}</p>
+      <button onClick={toggle}>{isActive ? "Pause" : "Start"}</button>
+      <button>Reset</button>
     </div>
   );
 };
